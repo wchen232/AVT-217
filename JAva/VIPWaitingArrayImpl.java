eed91f6550eac865ae5a7530d59ec393ff306bf3
wchen232 <54602965+wchen232@users.noreply.github.com>
2019-11-05 18:13:31 -0500

    11/5

--- JAva/VIPWaitingArrayImpl.java
+++ JAva/VIPWaitingArrayImpl.java
@@ -1,3 +1,21 @@
 public class VIPWaitingArrayImpl extends Parent implements Interface {
+  private int entryIndex;
+  private Array loopArray;
+  private int initialCapacity;
 
+  public VIPWaitingArrayImpl(){
+
+  }
+
+  public VIPWaitingArrayImpl(int capacity){
+
+  }
+
+  private void resizeCapacity(){
+
+  }
+  
+  @Override public String toString(){
+
+  }
 }
--- JAva/WaitingLoopArrayImpl.java
+++ JAva/WaitingLoopArrayImpl.java
@@ -1,3 +1,22 @@
 public class WaitingLoopArrayImpl extends Parent implements Interface {
+  private int entryIndex;
+  private int exitIndex;
+  private int initialCapacity;
+  loopArray;
 
+  public WaitingLoopArrayImpl(){
+
+  }
+
+  public WaitingLoopArrayImpl(int capacity){
+
+  }
+
+  private void resizeCapacity(){
+
+  }
+
+  @Override public String toString() {
+
+  }
 }
