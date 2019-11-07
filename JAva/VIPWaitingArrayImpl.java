459041f383f47695f70a3ec268badae74d7fdca9
wchen232 <54602965+wchen232@users.noreply.github.com>
2019-11-07 18:09:22 -0500

    Update VIPWaitingArrayImpl.java

--- JAva/VIPWaitingArrayImpl.java
+++ JAva/VIPWaitingArrayImpl.java
@@ -1,4 +1,4 @@
-public class VIPWaitingArrayImpl extends Parent implements Interface {
+public class VIPWaitingArrayImpl implements WaitingArrayInterface{
   private int entryIndex;
   private Array loopArray;
   private int initialCapacity;
@@ -8,8 +8,19 @@ public class VIPWaitingArrayImpl extends Parent implements Interface {
   }
 
   public VIPWaitingArrayImpl(int capacity){
-
+    this.loopArray = Object[capacity];
+    this.entryIndex = 0;
+    this.initialCapacity = capacity;
   }
+
+  public void add(G element);
+  public G remove() throws EmptyWaitingArrayException;
+  public void clear() throws EmptyWaitingArrayException;
+  public G getFirst() throws EmptyWaitingArrayException;
+  public G getLast() throws EmptyWaitingArrayException;
+  public boolean isEmpty();
+  public boolean isFull();
+  public String toString();
 
   private void resizeCapacity(){
 
