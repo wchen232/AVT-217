36144887f7f6a4aa048c733a2342b8eed962720a
wchen232 <54602965+wchen232@users.noreply.github.com>
2019-11-12 16:49:12 -0500

    11/12

---
+++ e17/jscss/main.js
@@ -0,0 +1,20 @@
+// everything must go in the document ready function
+$( document ).ready(function() {
+
+  // prints to the console which we can see through the inspector
+  console.log( 'ready!' );
+
+  // allows us to make an interaction on click
+  $('p').click(function() {
+
+    // grows the font size coded through jquery
+    // $('p').css('font-size','10vw');
+
+    // toggles a class we made in css. one click adds it, the next click removes it
+    $('p').toggleClass('grow');
+
+  });
+
+  // grows the font size when jquery loads
+  // $('p').css('font-size','10vw');
+});
