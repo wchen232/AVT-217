1dd25906e7c1c9abb32e728a5710953c0e50fbd8
wchen232 <54602965+wchen232@users.noreply.github.com>
2019-11-26 19:01:05 -0500

    11/26

--- p5/main.js
+++ p5/main.js
@@ -2,18 +2,18 @@ $(document).ready(function(){
   console.log("Document is ready");
 
   var links = [
-  'http://www.youtube.com/embed/P5l2wBEWNKc',
-  'http://www.youtube.com/embed/hLym7dcWw68',
-  'http://www.youtube.com/embed/ys099qG3HVY',
-  'http://www.youtube.com/embed/92wik18z78Y',
-  'http://www.youtube.com/embed/O2yRtgaldgg',
-  'http://www.youtube.com/embed/W8gaZvgO9ac',
-  'http://www.youtube.com/embed/JBSbyOzun0Y',
-  'http://www.youtube.com/embed/9EocrqxkvDk',
-  'http://www.youtube.com/embed/zN91WQzqQYc',
-  'http://www.youtube.com/embed/8kaW5xJ8WlI'
+  'https://www.youtube.com/embed/P5l2wBEWNKc',
+  'https://www.youtube.com/embed/hLym7dcWw68',
+  'https://www.youtube.com/embed/ys099qG3HVY',
+  'https://www.youtube.com/embed/92wik18z78Y',
+  'https://www.youtube.com/embed/O2yRtgaldgg',
+  'https://www.youtube.com/embed/W8gaZvgO9ac',
+  'https://www.youtube.com/embed/JBSbyOzun0Y',
+  'https://www.youtube.com/embed/9EocrqxkvDk',
+  'https://www.youtube.com/embed/zN91WQzqQYc',
+  'https://www.youtube.com/embed/8kaW5xJ8WlI'
   ];
-  
+
   randomVideo();
 
   function randomVideo() {
